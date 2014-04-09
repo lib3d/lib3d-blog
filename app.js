390740d4eb18c141301a111444fff501944883f6
lib3d <atondelier@gmail.com>
2014-04-10 01:11:35 +0200

    small seperation before real arthitecture

--- app.js
+++ app.js
@@ -1,50 +1,5 @@
+var serverLauncher = require('./serverLauncher');
+var server = serverLauncher();
+var restApi = require('./restApi');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-var mongoose = require('mongoose');
-
-var app = express();
-
-var models = require('./models');
-
-mongoose.connect('mongodb://localhost:27017/lib3d');
-
-// all environments
-app.configure(function() {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
-    app.use(express.static(path.join(__dirname, 'public')));
-});
-
-// development only
-app.configure('development', function() {
-    app.use(express.errorHandler());
-});
-
-var api = require('./routes/api');
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/posts', api.postList);
-app.post('/posts/:_id', api.postSave);
-app.post('/posts', api.postSave);
-app.delete('/posts/:_id', api.postDelete);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+restApi(server.application);
---
+++ restApi.js
@@ -0,0 +1,10 @@
+var api = require('./routes/api');
+var mongoose = require('mongoose');
+
+module.exports = function(app) {
+    mongoose.connect('mongodb://localhost:27017/lib3d');
+    app.get('/posts', api.postList);
+    app.post('/posts/:_id', api.postSave);
+    app.post('/posts', api.postSave);
+    app.delete('/posts/:_id', api.postDelete);
+}
