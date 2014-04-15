517b326f6d20d2289a7bde37b796e1e6f88bf8f5
atondelier <atondelier@lafourchette.com>
2014-04-15 15:02:43 +0200

    clean grunt tasks a bit

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,12 @@
 
 module.exports = function(grunt) {
     grunt.initConfig({
+        publicDir: './public',
+        viewsDir: './views',
         watch: {
             configFiles: {
                 files: ['./Gruntfile.js'],
-                tasks: ['clean', 'requirejs', 'manifest'],
+                tasks: ['assets:all'],
                 options: {
                     reload: true,
                     debounceDelay: 1000,
@@ -13,23 +15,23 @@ module.exports = function(grunt) {
                 }
             },
             javascripts: {
-                files: ['./public/javascripts/**/*.js'],
-                tasks: ['clean', 'requirejs', 'manifest'],
+                files: ['<%= publicDir %>/javascripts/**/*.js'],
+                tasks: ['assets:js'],
                 options: {
                     livereload: true,
                     debounceDelay: 1000
                 }
             },
             stylesheets: {
-                files: ['./public/stylesheets/**/*.less'],
-                tasks: ['manifest'],
+                files: ['<%= publicDir %>/stylesheets/**/*.less'],
+                tasks: ['assets:css'],
                 options: {
                     livereload: true,
                     debounceDelay: 1000
                 }
             },
             views: {
-                files: ['./views/**/*.jade'],
+                files: ['<%= viewsDir %>/**/*.jade'],
                 tasks: ['manifest'],
                 options: {
                     livereload: true,
@@ -40,9 +42,9 @@ module.exports = function(grunt) {
         requirejs: {
             compile: {
                 options: {
-                    appDir: './public/javascripts/',
+                    appDir: '<%= publicDir %>/javascripts/',
                     baseUrl: './',
-                    dir: './public/build/',
+                    dir: '<%= publicDir %>/build/',
                     optimize: 'uglify2',
                     preserveLicenseComments: false,
                     generateSourceMaps: true,
@@ -50,8 +52,8 @@ module.exports = function(grunt) {
                     removeCombined: true,
 
                     mainConfigFile: [
-                        'public/javascripts/common.js',
-                        'public/javascripts/blog.js'
+                        '<%= publicDir %>/javascripts/common.js',
+                        '<%= publicDir %>/javascripts/blog.js'
                     ],
 
                     modules: [
@@ -84,7 +86,7 @@ module.exports = function(grunt) {
         manifest: {
             generate: {
                 options: {
-                    basePath: 'public/',
+                    basePath: '<%= publicDir %>/',
                     cache: ['../'],
                     verbose: true,
                     timestamp: true,
@@ -95,19 +97,44 @@ module.exports = function(grunt) {
                     'components/requirejs/require.js',
                     'build/*.js',
                     'build/*.map',
-                    'components/requirejs/require.js',
                     'stylesheets/*.css'
                 ],
-                dest: 'public/manifest.appcache'
+                dest: '<%= publicDir %>/manifest.appcache'
             }
         },
-        clean: ['build']
+        less: {
+            development: {
+                options: {
+                    paths: ['<%= publicDir %>/stylesheets'],
+                    sourceMap: true
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= publicDir %>/stylesheets',
+                        src: ['*.less'],
+                        dest: '<%= publicDir %>/stylesheets',
+                        ext: '.css'
+                    }
+                ]
+            }
+        },
+        clean: {
+            js: ['build'],
+            css: ['<%= publicDir %>/stylesheets/*.css']
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-manifest');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.registerTask('default', ['clean', 'requirejs', 'manifest', 'watch']);
+    grunt.registerTask('assets:all', ['clean', 'requirejs', 'less:development', 'manifest']);
+
+    grunt.registerTask('assets:css', ['clean:css', 'less:development', 'manifest']);
+    grunt.registerTask('assets:js', ['clean:js', 'requirejs', 'manifest']);
+
+    grunt.registerTask('default', ['assets:all', 'watch']);
 };
