20cf63608ea20b2512aca1599765219ea2499c7c
atondelier <atondelier@lafourchette.com>
2014-06-04 17:45:16 +0200

    sprites

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,27 +4,22 @@
 module.exports = function(grunt) {
     grunt.initConfig({
 
-        // factorize paths in variables
-        dirs: {
-            'public': './public',
-            'views': './views'
-        },
-
         // require static configs from json files
-        requirejs: grunt.file.readJSON('grunt-requirejs.json'),
-        manifest: grunt.file.readJSON('grunt-manifest.json'),
-        less: grunt.file.readJSON('grunt-less.json'),
+        requirejs: grunt.file.readJSON('grunt/grunt-requirejs.json'),
+        manifest: grunt.file.readJSON('grunt/grunt-manifest.json'),
+        less: grunt.file.readJSON('grunt/grunt-less.json'),
+        sprite: grunt.file.readJSON('grunt/grunt-sprite.json'),
 
         // config clean tasks
         clean: {
             js: ['public/build/*'],
-            css: ['public/stylesheets/*.css']
+            css: ['public/build-stylesheets/*.css']
         },
 
         // config watch tasks
         watch: {
             configFiles: {
-                files: ['Gruntfile.js','grunt-*.json'],
+                files: ['Gruntfile.js','grunt/grunt-*.json'],
                 tasks: ['assets:all'],
                 options: {
                     reload: true
@@ -35,26 +30,28 @@ module.exports = function(grunt) {
                 tasks: ['assets:js']
             },
             stylesheets: {
-                files: ['public/stylesheets/**/*.less'],
+                files: ['public/stylesheets/**/*.less', '!public/stylesheets/sprites/*.less'],
+                tasks: ['assets:css']
+            },
+            sprites: {
+                files: ['public/images/sprites/**/*.*'],
                 tasks: ['assets:css']
             },
             views: {
                 files: ['views/**/*.jade'],
-                tasks: ['manifest'],
-                options: {
-                    livereload: true,
-                    debounceDelay: 300
-                }
+                tasks: ['manifest']
             },
             livereload: {
                 files: [
-                    'public/stylesheets/*.css',
-                    'public/build/*.js'
+                    'public/build-stylesheets/*.css',
+                    'public/build/*.js',
+                    'views/**/*.jade'
                 ],
                 options: {
                     livereload: true,
-                    debounceDelay: 300
+                    debounceDelay: 1000
                 }
+
             }
         }
     });
@@ -65,11 +62,13 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-manifest');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-spritesmith');
 
     // register tasks
-    grunt.registerTask('assets:css', ['clean:css', 'less:development', 'manifest']);
+    grunt.registerTask('assets:css', ['clean:css', 'sprite', 'less:development', 'manifest']);
     grunt.registerTask('assets:js', ['clean:js', 'requirejs', 'manifest']);
-    grunt.registerTask('assets:all', ['clean', 'requirejs', 'less:development', 'manifest']);
+    grunt.registerTask('assets:all', ['clean', 'requirejs', 'sprite', 'less:development', 'manifest']);
+
 
     // register default task
     grunt.registerTask('default', ['assets:all', 'watch']);
